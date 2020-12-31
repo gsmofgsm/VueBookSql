eef61a4076d82452b5593a7b5c8d896001af2ece
Qing Ma <gsmofgsm@hotmail.com>
2021-01-01 00:26:59 +0100

    apollo query categories

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import gql from "graphql-tag";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    categories: gql`
+      {
+        categories {
+          id
+          name
+        }
+      }
+    `,
+  },
+};
 </script>
