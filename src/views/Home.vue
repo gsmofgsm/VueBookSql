eb2870269f5a0e77b7d470c1e7d732723d33da2e
Qing Ma <gsmofgsm@hotmail.com>
2021-01-01 16:24:14 +0100

    Books all and featured

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
-    <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+    <ApolloQuery :query="categoriesQuery">
       <!-- The result will automatically updated -->
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <!-- Some content -->
         <div v-if="isLoading || loading">Loading...</div>
         <div v-else>
+          <a href="#" class="link-margin" @click="selectCategory('all')">All</a>
+          <a href="#" class="link-margin" @click="selectCategory('featured')"
+            >Featured</a
+          >
           <a
             href="#"
             v-for="category of data.categories"
@@ -19,61 +23,76 @@
       </template>
     </ApolloQuery>
 
-    <!-- <ApolloQuery :query="require('@/graphql/queries/Books.gql')">
-      <template slot-scope="{ result: { data, loading }, isLoading }">
-        <div v-if="isLoading || loading">Loading...</div>
-        <ul v-else>
-          <li href="#" v-for="book of data.books" :key="book.id">
-            {{ book.id }}. {{ book.title }}
-          </li>
-        </ul>
-      </template>
-    </ApolloQuery> -->
-
-    <ApolloQuery
-      :query="require('@/graphql/queries/Category.gql')"
-      :variables="{ id: selectedCategory }"
-    >
-      <!-- The result will automatically updated -->
-      <template slot-scope="{ result: { data, loading }, isLoading }">
-        <!-- Some content -->
-        <div v-if="isLoading || loading">Loading...</div>
-        <ul v-else>
-          <li href="#" v-for="book of data.category.books" :key="book.id">
-            {{ book.id }}. {{ book.title }}
-          </li>
-        </ul>
-      </template>
-    </ApolloQuery>
+    <div v-if="selectedCategory === 'all'">
+      <ApolloQuery :query="query">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading || loading">Loading...</div>
+          <ul v-else>
+            <li href="#" v-for="book of data.books" :key="book.id">
+              {{ book.id }}. {{ book.title }}
+            </li>
+          </ul>
+        </template>
+      </ApolloQuery>
+    </div>
+    <div v-else-if="selectedCategory === 'featured'">
+      <ApolloQuery :query="query" :variables="{ featured: true }">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading || loading">Loading...</div>
+          <ul v-else>
+            <li href="#" v-for="book of data.booksByFeatured" :key="book.id">
+              {{ book.id }}. {{ book.title }}
+            </li>
+          </ul>
+        </template>
+      </ApolloQuery>
+    </div>
+    <div v-else>
+      <ApolloQuery :query="categoryQuery" :variables="{ id: selectedCategory }">
+        <!-- The result will automatically updated -->
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <!-- Some content -->
+          <div v-if="isLoading || loading">Loading...</div>
+          <ul v-else>
+            <li href="#" v-for="book of data.category.books" :key="book.id">
+              {{ book.id }}. {{ book.title }}
+            </li>
+          </ul>
+        </template>
+      </ApolloQuery>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import gql from "graphql-tag";
+import categoryQuery from "@/graphql/queries/Category.gql";
+import categoriesQuery from "@/graphql/queries/Categories.gql";
+import booksQuery from "@/graphql/queries/Books.gql";
+import booksFeaturedQuery from "@/graphql/queries/BooksFeatured.gql";
 
 export default {
   name: "Home",
   components: {},
   data() {
     return {
-      selectedCategory: 1,
+      categoryQuery,
+      categoriesQuery,
+      booksQuery,
+      selectedCategory: "all",
+      query: booksQuery,
       categories: [],
     };
   },
-  apollo: {
-    // Simple query that will update the 'hello' vue property
-    categories: gql`
-      {
-        categories {
-          id
-          name
-        }
-      }
-    `,
-  },
   methods: {
     selectCategory(category) {
+      if (category === "all") {
+        this.query = booksQuery;
+      } else if (category === "featured") {
+        this.query = booksFeaturedQuery;
+      } else {
+        this.query = categoryQuery;
+      }
       this.selectedCategory = category;
     },
   },
