144477d8fe68b80d86135b66cab6e03cb76c29f2
Qing Ma <gsmofgsm@hotmail.com>
2021-01-01 17:02:12 +0100

    query Book

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is a book page {{ $route.params.id }}</h1>
+    <ApolloQuery
+      :query="require('@/graphql/queries/Book.gql')"
+      :variables="{ id: $route.params.id }"
+    >
+      <!-- The result will automatically updated -->
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <!-- Some content -->
+        <div v-if="isLoading || loading">Loading...</div>
+        <div v-else>
+          <div>{{ data.book.title }}</div>
+          <div>{{ data.book.author }}</div>
+          <img :src="data.book.image" alt="book cover" />
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
