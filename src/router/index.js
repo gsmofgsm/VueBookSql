4cb2ff9aeaccfcf1946afcbd82d0c22f6108194b
Qing Ma <gsmofgsm@hotmail.com>
2021-01-02 11:25:48 +0100

    Add a book

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,15 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
+    path: "/books/add",
+    name: "addBook",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AddBook.vue"),
+  },
+  {
     path: "/books/:id",
     name: "Book",
     // route level code-splitting
---
+++ src/views/AddBook.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="create">
+    <h1>Create Book</h1>
+    <form action="#" method="POST" @submit.prevent="addBook">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" v-model="title" />
+      </div>
+      <div class="form-group">
+        <label for="author">Author</label>
+        <input type="text" name="author" id="author" v-model="author" />
+      </div>
+      <div class="form-group">
+        <label for="image">Image</label>
+        <input type="text" name="image" id="image" v-model="image" />
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          v-model="description"
+        ></textarea>
+      </div>
+      <div class="form-group">
+        <label for="link">Link</label>
+        <input type="text" name="link" id="link" v-model="link" />
+      </div>
+      <div class="form-group">
+        <label for="featured">Featured</label>
+        <input
+          type="checkbox"
+          name="featured"
+          id="featured"
+          v-model="featured"
+        />
+      </div>
+      <div class="form-group">
+        <label for="category">Category</label>
+        <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+          <!-- The result will automatically updated -->
+          <template slot-scope="{ result: { data, loading }, isLoading }">
+            <!-- Some content -->
+            <div v-if="isLoading || loading">Loading...</div>
+            <select v-else v-model="category">
+              <option
+                v-for="category of data.categories"
+                :key="category.id"
+                :value="category.id"
+              >
+                {{ category.name }}
+              </option>
+            </select>
+          </template>
+        </ApolloQuery>
+      </div>
+      <div class="form-group">
+        <button type="submit">Add book</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import addBook from "@/graphql/mutations/AddBook.gql";
+export default {
+  data() {
+    return {
+      title: "",
+      author: "",
+      image: "",
+      description: "",
+      link: "",
+      featured: "",
+      category: "",
+    };
+  },
+  methods: {
+    addBook() {
+      this.$apollo
+        .mutate({
+          // Query
+          mutation: addBook,
+          // Parameters
+          variables: {
+            title: this.title,
+            author: this.author,
+            image: this.image,
+            link: this.link,
+            description: this.description,
+            featured: this.featured,
+            category: +this.category,
+          },
+        })
+        .then((data) => {
+          // Result
+          console.log(data);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          // Error
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-group {
+  margin-bottom: 32px;
+}
+input[type="text"] {
+  padding: 10px 14px;
+}
+button {
+  padding: 16px;
+  background: #027bff;
+  color: white;
+  border-radius: 5px;
+  font-size: 16px;
+}
+</style>
